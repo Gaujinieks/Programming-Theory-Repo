a0a3d2de73e2b78a14b645056e7a8eebce8007b9
Gaujinieks <janis.braivo@inbox.lv>
2022-06-20 05:52:13 +0300

    Movement progress
    Made it so player can't double jump using raycast and made separate method for jumping.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 500;
     public float jumpVelocity = 100.0f;
     private Vector3 _inputs = Vector3.zero;
-    private bool isGrounded;
+    //private bool isGrounded;
 
 
     // Start is called before the first frame update
@@ -17,29 +17,44 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnCollisionStay()
-    {
-        isGrounded = true;
-    }
+    //void OnCollisionStay()
+    //{
+    //    isGrounded = true;
+    //    Debug.Log(isGrounded);
+    //}
 
     private void Update()
     {
         _inputs.x = Input.GetAxis("Horizontal");
+        Jump(jumpVelocity);
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+    public bool IsGrounded()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 5))
         {
-            Debug.Log("Space was pressed");
-            rb.velocity = Vector3.up * jumpVelocity;
-            isGrounded = false;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void Jump(float velocity)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = Vector3.up * velocity;
+            //isGrounded = false;
         }
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if(_inputs.x > 0.1f || _inputs.x < -0.1f)
         {
-            rb.MovePosition(rb.position + _inputs * speed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * _inputs);
         }
     }
 
